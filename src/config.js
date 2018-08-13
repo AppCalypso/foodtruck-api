ff7538ec8dea734158c0714f67592b65cdbcdc8a
kencas <kcjojo8@gmail.com>
2018-08-13 17:35:30 +0100

    Fan

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    "database" : "mongodb://localhost:27017/foodtruck", 
-    //"database" : "mongodb://foodtrucker:trucker4real@ds113442.mlab.com:13442/foodtrucker",
+    //"database" : "mongodb://localhost:27017/foodtruck", 
+    "database" : "mongodb://foodtrucker:trucker4real@ds113442.mlab.com:13442/foodtrucker",
     "port" : process.env.PORT || 30000,
     "secretKey" : "66&&&888^^TTggg4$$$55g"
 }
